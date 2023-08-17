15e3cc8ebb6bc2a6e12fe47309c0dd9c0f3fc924
hun-jae <ihunjae94@gmail.com>
2023-08-17 15:57:54 +0900

    Add MemberController.java / Pathandler.java

---
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/util/PathHandler.java
@@ -0,0 +1,10 @@
+package net.ringo.ringoSwap.util;
+
+public class PathHandler
+{
+	// Main
+	public final static String MAIN = "main";
+	
+	// Member
+	public final static String MEMBER = "member";
+}
