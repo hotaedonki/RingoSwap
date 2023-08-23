a909bd4d868378d9b55827e8230ae3b172af8412
sirholee <sirholee@gmail.com>
2023-08-23 15:56:49 +0900

    < idCheck method complet(memberView/join.html과 memberView/login.html안의 join in 부문 모두 적용) >

--- ringoSwap/src/main/resources/static/js/login.js
+++ ringoSwap/src/main/resources/static/js/login.js
@@ -1,8 +1,22 @@
+$(document).ready(function(){
+	
+    //id체크 이벤트
+    $('#idCheck_Btn').click(memberIdCheck)
+})
+
+
+
 document.getElementById('signUp').addEventListener('click', function() {
     // 로그인 폼 표시
     document.getElementById('signUpForm').classList.remove('signInHidden');
 
     // 회원가입 폼 표시
     document.getElementById('loginForm').classList.add('signInHidden');
+    
 });
 
+function memberIdCheck() {
+	let id = $('#user_id').val();
+	
+	window.open("idCheck?user_id="+id, "ID중복확인", "width=500, height=300, left=150, top=50, scrollbars=no")
+}
