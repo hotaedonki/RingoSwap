caff6e30e74a72daa4d6f0e15fb7e3634a36a2b5
sirholee <sirholee@gmail.com>
2023-08-24 17:44:49 +0900

    < gamepage 진입 controller 작업 single - 사천성/단어조합/객관식/받아쓰기 complete >

--- ringoSwap/src/main/java/net/ringo/ringoSwap/service/GameService.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/service/GameService.java
@@ -1,5 +1,9 @@
 package net.ringo.ringoSwap.service;
 
+import net.ringo.ringoSwap.domain.SingleDifficulty;
+
 public interface GameService {
+
+	public SingleDifficulty difficultyCall(String user_id);
 
 }
