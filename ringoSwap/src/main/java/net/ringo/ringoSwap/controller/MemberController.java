2884802570fbaba435013cd5b0d66521db78eada
hun-jae <ihunjae94@gmail.com>
2023-08-25 18:24:02 +0900

    delete legacy code

--- ringoSwap/src/main/java/net/ringo/ringoSwap/controller/MemberController.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/controller/MemberController.java
@@ -93,15 +93,6 @@ public class MemberController
 		return "memberView/idCheck";
 	}
 	
-	@ResponseBody
-	@PostMapping(PathHandler.GETMEMBER)
-	public Boolean getMember(Member member)
-	{
-		Member m = service.getMember(member);
-		
-		return true;
-	}
-	
 	@PostMapping(PathHandler.EMAILCONFIRM)
 	public void emailConfirm(String email, HttpSession session) throws Exception
 	{
