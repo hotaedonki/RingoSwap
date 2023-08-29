66304dcad255796491d79d0aaded37869b92418f
sirholee <sirholee@gmail.com>
2023-08-29 15:59:59 +0900

    < feedMapper.xml 오류 수정 >

--- ringoSwap/src/main/java/net/ringo/ringoSwap/controller/FeedController.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/controller/FeedController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,15 +25,18 @@ public class FeedController
 	//피드서비스의 메인페이지로 이동하는 컨트롤러 메서드
 	
 	@GetMapping("feedMain")
-	public String feedMain()
+	public String feedMain(String feedTypeAll)
 	{
-		return "feed/feedMain";
+		if(feedTypeAll.isEmpty()) {
+			return "feedMain";					//feedTypeAll 값이 없으면 변수를 인계하지 않고 피드목록으로 이동한다
+		}
+		return "feedMain?feedType="+feedTypeAll;			//feedTypeAll 값이 존재하면
 	}
 
 	//<<<<<<<<<<<------[피드 출력 기능 시작]----------------------
 	//피드 목록 출력 controller 메서드(정렬방식:최근게시물/인기게시물)
 	@ResponseBody
-	@GetMapping("feedSelectAll")
+	@PostMapping("feedSelectAll")
 	public ArrayList<Feed> feedSelectAll(@RequestParam(name="feedTypeAll", defaultValue = "default") String feedTypeAll) {
 		ArrayList<Feed> feedList = new ArrayList<>();			//피드목록 출력용 배열 변수
 		
@@ -44,7 +48,24 @@ public class FeedController
 		
 		return feedList;
 	}
-
+	//특정 피드 게시글 페이지로 이동하는 controller 메서드
+	@GetMapping("feedRead")
+	public String feedRead(int feed_num,  String feedTypeAll) {
+		if(feed_num <=  0) {		//잘못된 feed 경로로 진입하면 되돌아가는 메서드
+			return "feedMain?feedType="+feedTypeAll;
+		}
+		
+		return "feedRead?feed_num="+feed_num;
+	}
+	//특정 피드 게시글에서 해당 게시글의 정보를 출력하는 controller 메서드
+	@ResponseBody
+	@PostMapping("feedSelectOne")
+	public Feed feedSelectOne(int feed_num) {
+		Feed feed = service.feedSelectOne(feed_num);
+		
+		return null;
+	}
+	
 	//----------------[피드 출력 기능 종료]----------->>>>>>>>>>>>
 
 }
