69813b38bebd230affed7fc55875da61f084cc81
hotaedonki <bigshipzz@gmail.com>
2023-09-13 15:04:32 +0900

    댓글 좋아요, 대댓글에 답글달기, 대댓글 삭제기능 구현

--- ringoSwap/src/main/java/net/ringo/ringoSwap/domain/MemberFollow.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/domain/MemberFollow.java
@@ -10,13 +10,13 @@ import lombok.NoArgsConstructor;
 public class MemberFollow {
 	int follower_num;			//자신을 팔로잉한 팔로워의 회원번호
 	int followee_num;			//자신이 팔로우한 팔로위의 회원번호
-	String follower_id;			//tagstorage에서 tag값을 담는 변수 / 미리 지정된 20개 전후의 변수가 담겨져 있고 유저는 수정/추가/삭제 불가.
-	String followee_id;
+	String follower_name;			//tagstorage에서 tag값을 담는 변수 / 미리 지정된 20개 전후의 변수가 담겨져 있고 유저는 수정/추가/삭제 불가.
+	String followee_name;
 	String original_profile;
 	String saved_profile;
 	public String toString()
 	{
-		return String.format("follower_num : {%d} / followee_num : {%d} / follower_id : {%s} / followee_id {%s} / original_profile {%s} / saved_profile {%s}"
-				,follower_num ,followee_num ,follower_id, followee_id, original_profile, saved_profile);
+		return String.format("follower_num : {%d} / followee_num : {%d} / follower_name : {%s} / followee_name {%s} / original_profile {%s} / saved_profile {%s}"
+				,follower_num ,followee_num ,follower_name, followee_name, original_profile, saved_profile);
 	}
 }
