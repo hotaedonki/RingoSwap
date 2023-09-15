f0e3f6e0f7ead0549f1436199ee80580f4bd1fa5
hun-jae <ihunjae94@gmail.com>
2023-09-15 19:11:48 +0900

    Revert "delete useless codes"
    This reverts commit 513637bd68b10a145021e4476a9b68420484ce80.

--- ringoSwap/src/main/java/net/ringo/ringoSwap/controller/ChatController.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/controller/ChatController.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import lombok.RequiredArgsConstructor;
@@ -94,6 +91,30 @@ public class ChatController
 		//chatEventHandlers...() 채팅방 서버 기능 관련 함수 추가하기
 		return "redirect:/";
 	}
+	
+	/*	나중엔 Ajax 형태로 받아오도록 하기.
+	@ResponseBody
+	@PostMapping(PathHandler.CREATEOPENCHATROOM)
+	public boolean createOpenChatroom(Chatroom chatRoom, @AuthenticationPrincipal UserDetails user)
+	{
+		log.debug("create open chat room . . .");
+		
+		if (chatRoom == null)
+		{
+			log.debug("chatRoom is null!");
+			return false;
+		}
+		
+		log.debug(chatRoom.toString());
+		
+		chatRoom.setHost_num(mService.memberSearchByIdReturnUserNum(user.getUsername()));
+		
+		boolean isSuccessCreateRoom = service.createOpenChatroom(chatRoom);
+		
+		return isSuccessCreateRoom;
+		//chatEventHandlers...() 채팅방 서버 기능 관련 함수 추가하기
+	}
+	*/
 	
 	@ResponseBody
 	@PostMapping(PathHandler.LOADCHATROOMS)
@@ -190,15 +211,74 @@ public class ChatController
 		return "chat/openChatRoom";
 	}
 	
-	@MessageMapping("/chat/openChatRoomEnter/")
-	public ResponseEntity<ChatCommon> sendMessage(@Payload ChatCommon message) 
+	/*
+	@ResponseBody
+    @PostMapping(PathHandler.SENDMESSAGE)
+    public Boolean sendMessage(ChatCommon cc, @AuthenticationPrincipal UserDetails user, MultipartFile upload) 
+    {
+		log.debug("send Message . . .");
+		
+    	// 채팅방의 고유 번호가 없는 경우 메시지를 보낼 수 없다.
+    	if (cc.getChatroom_num() <= 0)
+    	{
+    		log.debug("채팅방의 고유 번호가 필요합니다.");
+    		return false;
+    	}
+    	
+    	// 채팅 메시지 값이 없으면 메시지를 보낼 수 없다.
+    	if (cc.getMessage() == null || cc.getMessage().length() <= 0)
+    	{
+    		log.debug("채팅방 메시지가 비어있습니다.");
+    		return false;
+    	}
+    	
+    	cc.setUser_num(mService.memberSearchByIdReturnUserNum(user.getUsername()));
+    	
+    	if (upload != null && !upload.isEmpty())	// 사진이 있을때
+    	{
+    		String savedFile = FileService.saveFile(upload, uploadPath);
+    		cc.setOrigin_file(upload.getOriginalFilename());
+    		cc.setSaved_file(savedFile);
+    	}
+		
+    	log.debug("{}", cc.toString());
+    	
+    	int isSended = service.sendMessage(cc);
+    	
+    	if (isSended <= 0)
+    	{
+    		log.debug("메시지 정보 DB에 삽입 실패");
+    		return false;
+    	}
+		
+        return true;
+    }
+    */
+	
+	/*
+	@MessageMapping(PathHandler.MM_OPENCHATROOMENTER)
+	public void enterUser(@Payload ChatCommon chat, SimpMessageHeaderAccessor headerccessor)
 	{
-		log.debug("chatCommon Chatroom_num : {}", message.getChatroom_num());
-		
-	    // WebSocketSession 대신 SimpMessageSendingOperations를 사용하여 메시지를 전송합니다.
-		template.convertAndSend("/sub/chat/openChatroom/" + message.getChatroom_num(), "asdf");
-		
-		 return new ResponseEntity<ChatCommon>(HttpStatus.UNAUTHORIZED);
+		chat.setMessage(chat.getChat_num() + "님이 입장하셨습니다.");
+		template.convertAndSend("/sub/chat/openChatroom" + chat.getChatroom_num(), chat);
+	}
+	*/
+	
+	@MessageMapping("/chat/openChatRoomEnter/")
+	public void enterUser(@Payload ChatCommon chat, SimpMessageHeaderAccessor headerccessor)
+	{
+		chat.setMessage(chat.getChat_num() + "님이 입장하셨습니다.");
+		log.debug("openChatRoomEnter : {}", chat.getChatroom_num());
+		log.debug(chat.getMessage());
+		template.convertAndSend("/sub/chat/openChatroom/" + chat.getChatroom_num(), chat);
+	}
+	
+	@MessageMapping(PathHandler.MM_SENDMESSAGE)
+	public void sendMessage(@Payload ChatCommon chat)
+	{
+		log.info("chat : {}", chat.toString());
+		chat.setMessage(chat.getMessage());
+		template.convertAndSend("/sub/chat/openChatroom" + chat.getChatroom_num(), chat);;
 	}
 	
 	@ResponseBody
@@ -242,5 +322,49 @@ public class ChatController
         String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
 
         log.info("headAccessor : {}",headerAccessor);
+        
+        /*
+        // 채팅방 유저 -1
+        repository.decreaseUser(roomId);
+
+        //채팅방 유저 리스트에서 UUID 유저 닉네임 조회 및 리스트에서 유저 삭제
+        String userName = repository.getUserName(roomId, userUUID);
+        repository.deleteUser(roomId,userUUID);
+
+        if(userName != null){
+            log.info("User Disconnected : " + userName);
+
+            ChatDto chat = ChatDto.builder()
+                    .type(ChatDto.MessageType.LEAVE)
+                    .sender(userName)
+                    .message(userName + "님이 퇴장하였습니다.")
+                    .build();
+
+            template.convertAndSend("/sub/chat/room/" + roomId,chat);
+            */
 	}
+	
+	/*
+	 // 채팅에 참여한 유저 리스트 반환
+    @GetMapping("/chat/uselist")
+    @ResponseBody
+    public List<String> userList(String roomId){
+
+        return repository.getUserList(roomId);
+    }
+    */
+
+	/*
+    // 채팅에 참여한 유저 닉네임 중복 확인
+    @GetMapping("/chat/duplicateName")
+    @ResponseBody
+    public String isDuplicateName(@RequestParam("roomId")String roomId ,
+                                  @RequestParam("username")String username){
+
+        String userName = repository.isDuplicateName(roomId, username);
+        log.info("DuplicateName : {}", userName);
+
+        return userName;
+    }
+    */
 }
