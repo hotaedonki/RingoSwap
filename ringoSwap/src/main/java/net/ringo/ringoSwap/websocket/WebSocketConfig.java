f94d37653cb63c5d89a89358200518521f9e6913
hun-jae <ihunjae94@gmail.com>
2023-09-15 18:47:18 +0900

    Edit WebSocketConfig.java

--- ringoSwap/src/main/java/net/ringo/ringoSwap/websocket/WebSocketConfig.java
+++ ringoSwap/src/main/java/net/ringo/ringoSwap/websocket/WebSocketConfig.java
@@ -2,28 +2,35 @@
 package net.ringo.ringoSwap.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.ringo.ringoSwap.util.PathHandler;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
+@EnableWebSocketMessageBroker
 @Slf4j
-public class WebSocketConfig implements WebSocketConfigurer 
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer 
 {
-	// WebSocketHandler 에 관한 생성자 추가
-    private final ChatHandler chatHandler;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) 
+    {
+        registry.addEndpoint(PathHandler.RINGO_WS_STOMP)
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
 
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) 
-	{
-        // endpoint 설정 : /ws/chat
-        // 이를 통해서 ws://localhost:8080/ws/chat 으로 요청이 들어오면 websocket 통신을 진행합니다.
-        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
-		log.debug("Active registerWebSocketHandlers : {}", registry);
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) 
+    {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
