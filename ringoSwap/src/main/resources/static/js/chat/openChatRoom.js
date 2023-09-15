fc69e5732d4857477d61c07fb445ab9bee3f4a20
hun-jae <ihunjae94@gmail.com>
2023-09-15 19:32:47 +0900

    organize code

--- ringoSwap/src/main/resources/static/js/chat/openChatRoom.js
+++ ringoSwap/src/main/resources/static/js/chat/openChatRoom.js
@@ -28,6 +28,8 @@ function init()
 	url = new URL(location.href).searchParams;
 }
 
+// 원하는 키 값을 getElementById('E_ID').value 형태로 받아오는 경우 value 안에 키를 찾아줌
+// 찾을 키(키 이름, 가져온 value값) 
 function findValueByKey(key, inputString) 
 {
 	// 정규 표현식 패턴: "key : {value}" 형태를 찾음
@@ -47,29 +49,30 @@ function findValueByKey(key, inputString)
 	}
 }
 
+// 소켓을 만들어주는 역할을 하는 함수
 function connect()
 {
 	// 연결하고자하는 Socket 의 endPoint
     let socket = new SockJS('/ringo/ws-stomp');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, onConnected, onError);
-    
     //alert("연결 완료");
 }
 
 function onConnected() 
 {
 	// ChatCommon 객체를 생성
-	const chatCommon = {
+	const chatCommon = 
+	{
 		type: 'ENTER', // MessageType.ENTER와 동일
-		chat_num: 123, // 채팅 번호 (원하는 값으로 변경)
-		user_num: 456, // 사용자 번호 (원하는 값으로 변경)
-		chatroom_num: 789, // 채팅방 번호 (원하는 값으로 변경)
-		message: "입장합니다", // 메시지 내용 (원하는 내용으로 변경)
-		inputdate: "2023-09-12", // 입력 날짜 (원하는 날짜로 변경)
-		origin_file: "", // 원본 파일 (원하는 값으로 변경)
-		saved_file: "", // 저장된 파일 (원하는 값으로 변경)
-		photo_size: 0 // 사진 크기 (원하는 값으로 변경)
+		chat_num: "", // 채팅 번호
+		user_num: myUserNum, // 사용자 번호
+		chatroom_num: chatroomNum, // 채팅방 번호
+		message: "", // 메시지 내용
+		inputdate: "", // 입력 날짜
+		origin_file: "", // 원본 파일
+		saved_file: "", // 저장된 파일
+		photo_size: 0 // 사진 크기
 	};
 
 	// stompClient.send()를 사용하여 메시지 전송
@@ -80,17 +83,22 @@ function onConnected()
 	stompClient.subscribe('/sub/chat/openChatRoom/' + chatroomNum, onMessageReceived);
 }
 
-function onError(error) {
+// 접속 실패 후, 에러 발생시 실행하는 함수
+function onError(error) 
+{
     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
 }
 
 // 메시지 전송때는 JSON 형식을 메시지를 전달한다.
-function sendMessage(event) {
+function sendMessage(event) 
+{
     let messageContent = messageInput.value.trim();
 
-    if (messageContent && stompClient) {
-        let chatMessage = {
+    if (messageContent && stompClient) 
+    {
+        let chatMessage = 
+        {
             "roomId": roomId,
             sender: username,
             message: messageInput.value,
